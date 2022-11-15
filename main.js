3b97cde941e14b0f264c4d9459b9f746aeddae7f
Jhoabe Leão <jhoabe.leao@hotmail.com>
2022-11-15 10:24:27 -0300

    bandeiras

--- main.js
+++ main.js
@@ -25,7 +25,9 @@ function createCard(date, day, games) {
 document.querySelector('#cards').innerHTML =
   createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) + 
 
-  createCard("21/11", "segunda", createGame("england", "10:00", "iran")) +
+  createCard("21/11", "segunda", createGame("england", "10:00", "iran") + 
+    createGame("senegal", "13:00", "netherlands") +
+    createGame("england", "10:00", "wales")) +
    
   createCard("22/11", "Terça", createGame("argentina", "07:00", "saudi-arabia")) +
     
