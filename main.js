cd782d28342056a0e859292f00b80adcdeef4345
Jhoabe Leão <jhoabe.leao@hotmail.com>
2022-11-20 11:19:11 -0300

    Teste

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML =
-  createCard("20/11", "domingo", createGame("qatar", "13:00", "grupo c", "ecuador")) + 
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "GRUPO C", "ecuador")) + 
 
   createCard("21/11", "segunda", createGame("england", "10:00", "iran") + 
     createGame("senegal", "13:00", "netherlands") +
